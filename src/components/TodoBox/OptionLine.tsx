27fe34320bae1692575cfb8d75a239f53a08ec89
leesj8115 <leesj8115@skuniv.ac.kr>
2022-11-21 19:34:30 +0900

    카테고리 선택에 맞는 todo를 관리할 수 있도록 상태 추가, 리렌더링 스트링 분리

--- src/components/TodoBox/OptionLine.tsx
+++ src/components/TodoBox/OptionLine.tsx
@@ -1,15 +1,15 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { LIST_OPTION } from "../../CONST_VALUE";
 import { TODO } from "./TodoBox";
 
 type optionLineProps = {
     todoList: TODO[],
-    setTodoList: Dispatch<SetStateAction<TODO[]>>
+    // setTodoList: Dispatch<SetStateAction<TODO[]>>
 };
 
-const OptionLine = ({todoList, setTodoList}: optionLineProps) => {
-    const [category, setCategory] = useState<string>(LIST_OPTION[0]);
-
+const OptionLine = ({todoList}: optionLineProps) => {
+    const [category, setCategory] = useState<string>(LIST_OPTION[0]);   // 카테고리
+    const [categoryItem, setCategoryItem] = useState<TODO[]>([]);       // 카테고리에 의해 분류된 아이템
 
     // 하단의 옵션 클릭시 category 값을 변경하는 이벤트
     const optionClickEvent = (event: React.MouseEvent<HTMLLIElement>) => {
@@ -20,9 +20,12 @@ const OptionLine = ({todoList, setTodoList}: optionLineProps) => {
         setCategory(text ?? LIST_OPTION[0]);
     }
 
+    // 아이템 갯수에 따른 안내 메세지
+    const cntText = `${todoList.length} ${todoList.length === 1 ? 'item' : 'items'} left`;
+
     return (
         <div className='box-size option-line'>
-            <span>{`${todoList.length} ${todoList.length === 1 ? 'item' : 'items'} left`}</span>
+            <span>{cntText}</span>
             <ul className="option">
                 {LIST_OPTION.map(e => {
                     return (
